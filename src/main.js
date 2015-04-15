b67b7f90c323480666b61d92e52a7f5f3495b2b6
Aleksi Pekkala <aleksipekkala@hotmail.com>
2015-04-15 14:19:28 -0300

    block animations

--- src/main.js
+++ src/main.js
@@ -14,6 +14,9 @@ var blockWidth;
 var blockMargin = 2;
 
 var matrix;
+var nextMatrixPos;
+
+var fillStep = 500;
 
 
 var svg = d3.select(elem[0]).append('svg')
@@ -21,9 +24,11 @@ var svg = d3.select(elem[0]).append('svg')
     .attr('height', height);
 
 function initMatrix(n, m, gapPenalty) {
+    matrix = [];
+    nextMatrixPos = {x: 1, y: 1};
+
     var cols = n + 1;
     var rows = m + 1;
-    matrix = [];
     width = elem.width();
     blockWidth = (width - margin.right - margin.left) / cols - blockMargin * 2;
 
@@ -44,34 +49,61 @@ function initMatrix(n, m, gapPenalty) {
 
     svg
         .attr('width', width)
-        .attr('height', (blockWidth + blockMargin * 2) * m + margin.top + margin.bottom);
+        .attr('height', (blockWidth + blockMargin * 2) * rows + margin.top + margin.bottom);
 
     blocksGroup = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 }
 
-function update() {
+function fill() {
+    // Fill the next empty matrix position
+    matrix[nextMatrixPos.y][nextMatrixPos.x] = 1;
+    if (nextMatrixPos.x === matrix[0].length - 1) {
+        nextMatrixPos = {x: 1, y: nextMatrixPos.y + 1};
+    } else {
+        nextMatrixPos = {x: nextMatrixPos.x + 1, y: nextMatrixPos.y};
+    }
+
+
+    // Transform matrix data into visualization data
     var data = [];
-    matrix.forEach(function(rows, i) {
-        rows.forEach(function(d, j) {
-            data.push({
-                x: j,
-                y: i,
-                val: d,
-            });
+    matrix.forEach(function(row, i) {
+        row.forEach(function(d, j) {
+            if (d !== null) {
+                data.push({
+                    x: j,
+                    y: i,
+                    val: d,
+                });
+            }
         });
     });
 
+    // Update visualization
     blocksGroup.selectAll('rect')
-        .data(data)
+        .data(data, function(d) { return d.x + '-' + d.y; })
         .enter()
         .append('rect')
             .attr('x', function(d) { return d.x * (blockWidth + blockMargin * 2) + blockMargin; })
             .attr('y', function(d) { return d.y * (blockWidth + blockMargin * 2) + blockMargin; })
             .attr('width', blockWidth)
             .attr('height', blockWidth)
-            .attr('fill', 'red');
+            .attr('fill', getBlockColor)
+            .attr('opacity', 0)
+            .transition().duration(1000)
+            .attr('opacity', 1);
+
+    // Check if finished, otherwise fill more
+    if (nextMatrixPos.y >= matrix.length) {
+        console.log('traceback TODO');
+    } else {
+        setTimeout(fill, fillStep);
+    }
+}
+
+function getBlockColor(d) {
+    return 'steelblue';
 }
 
 initMatrix(10, 6, -4);
-update();
+fill();

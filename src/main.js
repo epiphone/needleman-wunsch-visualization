ffb3ba3f11b3fe7ebbc809f1b16aaf7e4dd0ac86
Aleksi Pekkala <aleksipekkala@hotmail.com>
2015-04-15 13:23:58 -0300

    drawing blocks

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ var width;
 var height;
 var margin = {top: 10, left: 10, bottom: 10, right: 10};
 
+var blocksGroup;
 var blockWidth;
 var blockMargin = 2;
 
@@ -24,7 +25,7 @@ function initMatrix(n, m, gapPenalty) {
     var rows = m + 1;
     matrix = [];
     width = elem.width();
-    blockWidth = (width - margin.right - margin.left) / cols;
+    blockWidth = (width - margin.right - margin.left) / cols - blockMargin * 2;
 
     for (var i = 0; i < rows; i++) {
         matrix.push([]);
@@ -41,11 +42,36 @@ function initMatrix(n, m, gapPenalty) {
         }
     }
 
-    console.log(matrix);
-
     svg
         .attr('width', width)
-        .attr('height', blockWidth * m + margin.top + margin.bottom);
+        .attr('height', (blockWidth + blockMargin * 2) * m + margin.top + margin.bottom);
+
+    blocksGroup = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+}
+
+function update() {
+    var data = [];
+    matrix.forEach(function(rows, i) {
+        rows.forEach(function(d, j) {
+            data.push({
+                x: j,
+                y: i,
+                val: d,
+            });
+        });
+    });
+
+    blocksGroup.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+            .attr('x', function(d) { return d.x * (blockWidth + blockMargin * 2) + blockMargin; })
+            .attr('y', function(d) { return d.y * (blockWidth + blockMargin * 2) + blockMargin; })
+            .attr('width', blockWidth)
+            .attr('height', blockWidth)
+            .attr('fill', 'red');
 }
 
 initMatrix(10, 6, -4);
+update();
